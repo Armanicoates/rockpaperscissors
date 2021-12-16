124b0d65216d8721bcaf614d6ca41a94307d562c
Armanicoates <armanicoates@yahoo.com>
2021-12-16 13:43:30 -0500

    1 file modified

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ let computerSelection = computerPlay();
 
 function computerPlay() {
   let arr = ['rock', 'paper', 'scissors'];
-  let randomArr = Math.floor(Math.random() * 3);
-  return arr[randomArr];
+  let randomArr = Math.floor(Math.random() *3);
+  return arr[randomArr] 
+  
 }
-console.log(computerPlay() ) ;
-function playGame(playerSelection, computerSelection) {}
+
+function playGame(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+  console.log("its a tie");
+  } else if 
+  
+   (computerSelection === 'rock' && playerSelection === 'scissors' ||
+   computerSelection === 'scissors' && playerSelection === 'paper' ||
+  computerSelection === 'paper' && playerSelection === 'rock') {
+  
+   console.log(
+     'you lose!' + computerSelection + 'beats' + playerSelection + '.'
+     );
+} else {
+  console.log(
+    'you win!' + playerSelection + 'beats' + computerSelection + '.'
+  );
+  // console.log('you win!' + playerSelection+'beats' +computerSelection +
+  '.';
+  }
+}  
+
+  
 
 playGame(playerSelection, computerSelection);
